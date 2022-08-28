cbba73da5c16ca7e60b22b660803a2e567cc07ae
faazizpro <fahadafs11@gmail.com>
2022-08-28 16:27:45 +0600

    Understood kanye quote api

--- js/quots.js
+++ js/quots.js
@@ -1,5 +1,12 @@
 
 const loadQuote = () => {
-    console.log('Loading quots');
+    fetch('https://api.kanye.rest/')
+    .then (res => res.json())
+    .then ( data => displayQuote(data));
 }
-loadQuote();
+
+const displayQuote = quote => {
+    const blockQuote = document.getElementById('quote');
+    console.log(quote);
+    blockQuote.innerText = quote.quote;
+}
